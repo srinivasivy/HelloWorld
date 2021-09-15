1d4a886b3f654aeed62fd8698ddf7021016ee1ab
srinivasivy <srinivas_ivy@yahoo.com>
2021-09-15 14:03:06 +0530

    Hello World

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,15 @@
-public class HelloWorld {
-   public static void main(String[] args) {
-      System.out.println("Hello World");
-   }
+/* #!/bin/bash */
+/* # public class HelloWorld with No public only class */
+
+ class HelloWorld {
+
+   public static void main(String[] args){
+
+         for (int i=1;i<=10;i++){
+ 
+              System.out.println("Hello World only class... " +i);
+	}   
+
+     }
 }
 
